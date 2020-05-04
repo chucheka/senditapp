0d1de1a37d68c9577b45e2c60cca13b4fb72dd02
ucheka22 <ryanucheka@gmail.com>
2020-05-04 12:13:27 +0100

    first commit

---
+++ src/main/java/com/neulogics/senditapp/SenditappApplication.java
@@ -0,0 +1,13 @@
+package com.neulogics.senditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenditappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenditappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neulogics/senditapp/SenditappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neulogics.senditapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenditappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
