2366babb9f46383ef8029c3fa9070fc883a82814
ucheka22 <ryanucheka@gmail.com>
2020-05-04 17:51:43 +0100

    bug fixes

--- src/main/java/com/neulogics/senditapp/SenditappApplication.java
+++ src/main/java/com/neulogics/senditapp/SenditappApplication.java
@@ -2,19 +2,11 @@ package com.neulogics.senditapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
+
 @SpringBootApplication
 public class SenditappApplication {
-	
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello world";
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SenditappApplication.class, args);
