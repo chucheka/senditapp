4f8777ab3600e5cfac1f7e280062eae6444309f7
ucheka22 <ryanucheka@gmail.com>
2020-05-07 04:55:55 +0100

    bug fixes

--- src/main/java/com/neulogics/senditapp/helpers/MyPostConstructBean.java
+++ src/main/java/com/neulogics/senditapp/helpers/MyPostConstructBean.java
@@ -1,44 +1,44 @@
-package com.neulogics.senditapp.helpers;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.neulogics.senditapp.models.ERole;
-import com.neulogics.senditapp.models.Role;
-import com.neulogics.senditapp.respository.RoleRepository;
-
-@Component
-public class MyPostConstructBean {
-	@Autowired
-	private RoleRepository roleRepo;
-	
-	@PostConstruct
-	  void postConstruct(){
-	   roleRepo.save(new Role(ERole.ROLE_USER));
-	   roleRepo.save(new Role(ERole.ROLE_ADMIN));
-	   
-	   
-	  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//package com.neulogics.senditapp.helpers;
+//
+//import javax.annotation.PostConstruct;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import com.neulogics.senditapp.models.ERole;
+//import com.neulogics.senditapp.models.Role;
+//import com.neulogics.senditapp.respository.RoleRepository;
+//
+//@Component
+//public class MyPostConstructBean {
+//	@Autowired
+//	private RoleRepository roleRepo;
+//	
+//	@PostConstruct
+//	  void postConstruct(){
+//	   roleRepo.save(new Role(ERole.ROLE_USER));
+//	   roleRepo.save(new Role(ERole.ROLE_ADMIN));
+//	   
+//	   
+//	  }
+//
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
